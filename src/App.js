b10d85f9b06d1553006e0a7a4f5c0a6835141256
zero <ahmadfirdosh008@gmail.com>
2023-01-07 11:26:53 +0530

    first commit for the setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import LOGO from "./components/log"
+import Nav from "./container/nav_bar_side"
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-10">
+      <LOGO/>
+      <Nav/>
     </div>
-  );
+    
+  )
 }
-
-export default App;
---
+++ src/components/list.js
@@ -0,0 +1,5 @@
+export default function App() {
+    return (
+        <li className='my-10'><a href='#' className=' text-3xl'> <IoHomeOutline/></a></li>  
+    )
+  }
